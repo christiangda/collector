bfaae1328861b143b44644365937861b2796bf05
Christian Gonzalez Di Antonio <christiangda@gmail.com>
2018-02-10 21:05:31 +0100

    improved

--- scripts/lua/put_in_redis.lua
+++ scripts/lua/put_in_redis.lua
@@ -1,37 +1,56 @@
 local redis = require "resty.redis"
-local red = redis:new()
-local channel = "raw_data" 
+local pb = require "pb"
+local protoc = require "protoc"
 
-local sock = assert(ngx.req.socket(true))
-local data = sock:receive()  -- read a line from downstream
+local CHANNEL = "raw_data"
+local remote_addr = ngx.var.remote_addr
 
--- Data block
-if data == "thunder!" then
-    ngx.say("flash!")  -- output data
-else
-    ngx.say("data = ", data)
+local sock, err = ngx.req.socket(true)
+if not sock then
+    ngx.log(ngx.ERR, "failed to get socket connection: ", err)
+    local ok, err = sock.close()
+    if not ok then
+        ngx.log(ngx.ERR, "failed to close socket connection: ", err)
+    end
 end
 
--- Redis block
+local red = redis:new()
 red:set_timeout(1000) -- 1 sec
-
 local ok, err = red:connect("unix:/tmp/redis.sock")
 if not ok then
-    ngx.say("failed to connect: ", err)
-    return
+    ngx.log(ngx.ERR, "failed to connect to redis: ", err)
+    
+    -- close conection to avoid data loss
+    local ok, err = sock:shutdown("send")
+    if not ok then
+        ngx.log(ngx.ERR, "failed to shutdown: ", err)
+        return
+    end
 end
 
-ok, err = red:publish(channel, data)
+-- Data block
+-- read a line from downstream
+local data = sock:receive()
+
+ok, err = red:publish(CHANNEL, data)
 if not ok then
-    ngx.say("failed to set dog: ", err)
+    ngx.log(ngx.ERR, "failed on redis to publish in clannel: ", err)
     return
+elseif ok == 0 then
+    ngx.log(ngx.ERR, "no susbcriber exist on channel: ", CHANNEL)
+    
+    -- close conection to avoid data loss
+    local ok, err = sock:shutdown("send")
+    if not ok then
+        ngx.log(ngx.ERR, "failed to shutdown: ", err)
+        return
+    end
 end
 
-ngx.say("set result: ", ok)
 
 -- or just close the connection right away:
-local ok, err = red:close()
-if not ok then
-    ngx.say("failed to close: ", err)
-    return
-end
+--local ok, err = red:close()
+--if not ok then
+--    ngx.say("failed to close: ", err)
+--    return
+--end
